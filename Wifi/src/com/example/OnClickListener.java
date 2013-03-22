35d3d36b5a8f32612ef77cd365297ca26a271253
Nguyen Truong Thinh <nguyentruongthinhcp@gmail.com>
2013-03-22 23:44:19 +0700

    new commit

---
+++ Wifi/src/com/example/OnClickListener.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+public class OnClickListener implements OnCheckedChangeListener {
+
+	@Override
+	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
