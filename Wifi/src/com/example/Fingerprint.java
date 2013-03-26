589489152ecff3a4eeaf809e64687a7893393253
anhtrung93 <trunganhdinh@gmail.com>
2013-03-26 14:22:11 +0700

    add comments

--- Wifi/src/com/example/Fingerprint.java
+++ Wifi/src/com/example/Fingerprint.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.example;
 
 import java.util.List;
@@ -10,26 +7,25 @@ import android.net.wifi.ScanResult;
 /**
  * @author anhtrung93
  *
+ *Class 'Fingerprint' Overview:
+ *List of all WiFiSignature the device gets at a time
+ * which was arranged for future implementation
+ *
  */
 public class Fingerprint {
+	/********************Fields*****************************/
+	/********************Public Methods*********************/
 	private int size;
 	private WiFiSignature [] WiFiList;
 	
-	public Fingerprint(List<ScanResult> WiFiList){
-		if (WiFiList == null){
-			this.size = -1;
-			return;
-		}
-		this.size = WiFiList.size();
-		this.WiFiList = new WiFiSignature[this.size];
-		for (int i = 0; i < this.size; i++){
-			this.WiFiList[i] = new WiFiSignature(WiFiList.get(i));
-		}
-		
-		this.reorganize();		
-	}
-	
-	private void reorganize(){
+	/********************Private Methods********************/
+	/**
+	 * Method: sort()
+	 * Input:  'this'
+	 * Output: 'this' with the array of WiFiSignature
+	 * 	- sorted by their BSSID values
+	 */
+	private void sort(){
 		for (int i = 0; i < this.size - 1; i++){
 			String BSSID1 = this.WiFiList[i].getBSSID();
 			for (int j = i+1; j < this.size; j++){
@@ -44,10 +40,44 @@ public class Fingerprint {
 		}
 	}
 	
+	/********************Public Methods*********************/
+	/**
+	 * Constructor: Fingerprint(List<ScanResult> WiFiList)
+	 * Input:  'this' +  a list of ScanResult
+	 * Output: 'this' with the array of WiFiSignature
+	 * 	- constructed from the WiFiList
+	 *  - the array was sorted by the BSSID value
+	 */
+	public Fingerprint(List<ScanResult> WiFiList){
+		if (WiFiList == null){
+			this.size = -1;
+			return;
+		}
+		this.size = WiFiList.size();
+		this.WiFiList = new WiFiSignature[this.size];
+		for (int i = 0; i < this.size; i++){
+			this.WiFiList[i] = new WiFiSignature(WiFiList.get(i));
+		}
+		
+		this.sort();		
+	}
+	
+	/**
+	 * Method: getSize()
+	 * Input: this
+	 * Output: the number of the WiFiSignature
+	 */
 	public int getSize(){
 		return this.size;
 	}
 	
+	/**
+	 * Method: toString()
+	 * Input: this
+	 * Output: a string with this.size line group
+	 * - A line group shows the information about 
+	 * - a WiFiSignature
+	 */
 	public String toString(){
 		String tempString = "";
 		for (int i = 0; i < this.size; i++){
--- Wifi/src/com/example/WiFiSignature.java
+++ Wifi/src/com/example/WiFiSignature.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.example;
 
 import android.net.wifi.ScanResult;
@@ -17,21 +14,41 @@ public class WiFiSignature {
 	private String BSSID;		//Address of the access point
 	private int RSS;			//Signal Level in dBm
 	
-	/********************Methods*****************************/
+	/********************Private Methods********************/
+	/********************Public Methods*********************/
 	public WiFiSignature(ScanResult scanResult){
 		this.BSSID = scanResult.BSSID;
 		this.RSS = scanResult.level;
 	}
 	
-	//
+	/**
+	 * Method: getBSSID()
+	 * Input: this
+	 * Output: String in the form of 
+	 * - a six-byte MAC address: XX:XX:XX:XX:XX:XX
+	 * - 'X : is an alpha-number
+	 */
 	public String getBSSID(){
 		return this.BSSID;
 	}
 	
+	/**
+	 * Method: getRSS()
+	 * Input: this
+	 * Output: the signal strength integer
+	 * -  in range from -100 to ???
+	 */
 	public int getRSS(){
 		return this.RSS;
 	}
 	
+	/**
+	 * Method: toString()
+	 * Input: this
+	 * Output: a string with 2 lines
+	 * - First line: the BSSID string
+	 * - Second line: the signal strength
+	 */
 	public String toString(){
 		return "BSSID: " + this.BSSID + "\nRSS: " + this.RSS + "\n";
 	}	
