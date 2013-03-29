33055ca2194572ade3987f31089d455d95ba45f2
bvuong1993 <bvuong1993@gmail.com>
2013-03-29 23:55:36 +0700

    Update AddRequest.java

--- Wifi/src/com/example/AddRequest.java
+++ Wifi/src/com/example/AddRequest.java
@@ -1,1 +1,12 @@
 // @author: bvuong1993
+
+package com.example;
+
+// This implements a specific type of a request
+// Use this request type to add a fingerprint to a server
+
+public class AddRequest extends Request {
+    public AddRequest(Fingerprint fp) {
+        super(fp);
+    }  
+}
