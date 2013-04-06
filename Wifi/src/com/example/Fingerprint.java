06fb2a8baf079c29f270ca1873828334ab718605
hidrodo <hidrodo@gmail.com>
2013-04-06 23:20:23 +0700

    fix small typing errors

--- Wifi/src/com/example/Fingerprint.java
+++ Wifi/src/com/example/Fingerprint.java
@@ -9,23 +9,19 @@ import android.net.wifi.ScanResult;
  * @author anhtrung93
  * 
  *         Class Fingerprint overview: Objects of this class keep a list of all
- *         wifiSignature which a device scan gets and a label of its location
+ *         WifiSignature which a device scan gets and a label of its location
  * 
  */
-<<<<<<< HEAD
 public class Fingerprint{
 	/********************Fields*****************************/
 	private int size;
-	private WiFiSignature [] WiFiList;
+	private WifiSignature [] WiFiList;
 	String label;	//name of the room or location
 	
 	/********************Private Methods********************/
-=======
-public class Fingerprint  {
 	private WifiSignature[] wifiList; //the list of WifiSignature objects
 	String locationLabel; //the label assigned by users
 
->>>>>>> c67941791b50119da79e6d8eaf74bafe981adf24
 	/**
 	 * Constructor: Fingerprint(List<ScanResult>)
 	 * Input: a list of ScanResult objects
@@ -35,11 +31,8 @@ public class Fingerprint  {
 	 * - this.wifiList is sorted and filtered (watch sort() and filter())
 	 * - this.locationLabel is initialized with "Unknown location"
 	 */
-<<<<<<< HEAD
 	
-	public void sort(){
-		Arrays.sort(this.WiFiList);
-=======
+	
 	public Fingerprint(List<ScanResult> listOfScanResults) {
 		int size = listOfScanResults.size();		
 		this.wifiList = new WifiSignature[size];
@@ -51,7 +44,6 @@ public class Fingerprint  {
 		this.filter();
 		
 		this.locationLabel = "Unknown location";
->>>>>>> c67941791b50119da79e6d8eaf74bafe981adf24
 	}
 
 	/**
@@ -77,6 +69,7 @@ public class Fingerprint  {
 	 * - this.locationLabel is initialized with the String
 	 * Note: this method is used to test!!!
 	*/
+
 	public Fingerprint(WifiSignature[] initWifiList,
 			String initLocationLabel) {
 		super();
@@ -105,9 +98,9 @@ public class Fingerprint  {
 	//This method needs to be changed in the future release
 	public float differFrom(Fingerprint anotherFingerprint) {
 		WifiSignature[] thisWifiList = this.getWifiList();
-		WifiSignature[] anotherWifiList = anotherFingerprint.getwifiList();
+		WifiSignature[] anotherWifiList = anotherFingerprint.getWifiList();
 		int thisSize = this.getSize();
-		int anotherSize = otherFingerprint.getSize();
+		int anotherSize = anotherFingerprint.getSize();
 		
 		int count = 0; // Number of matched WifiSignature
 		int difference = 0;
@@ -168,37 +161,10 @@ public class Fingerprint  {
 	 * - the array of WifiSignature objects
 	 * - sorted by their BSSID values
 	 */
-<<<<<<< HEAD
-	public float differFrom(Fingerprint another) {		
-		WiFiSignature[] list1 = this.getWiFiList();
-		WiFiSignature[] list2 = another.getWiFiList();
-		int size1 = this.getSize();
-		int size2 = another.getSize();
-		int count = 0;
-		int diffentFingerprints = 0;
-		
-		int i = 0, j = 0;
-		while (i < size1 && j < size2) {
-			WiFiSignature s1 = list1[i];
-			WiFiSignature s2 = list2[i];
-			int d = s1.compareTo(s2);
-			if (d == 0) {
-				count++;
-				int sd = s1.getRSS() - s2.getRSS();
-				diffentFingerprints += sd*sd;
-			} else if (d < 0) {
-				i++;
-			} else {
-				j++;
-			}
-		}
-		return (float)diffentFingerprints / ((float) count / (size1+size2));			
-=======
-	private void sort() {
-		Arrays.sort(this.wifiList);
->>>>>>> c67941791b50119da79e6d8eaf74bafe981adf24
+	public void sort(){
+		Arrays.sort(this.WiFiList);
 	}
-
+	
 	/**
 	 * Method: toString()
 	 * Input: this object
